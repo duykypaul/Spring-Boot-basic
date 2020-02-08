6fb747888ba824eb4c82c491b3e7a99b6c04f9da
duykypaul <duykypaul@gmail.com>
2020-02-08 12:42:25 +0700

    init project

---
+++ src/main/java/com/duykypaul/api/Application.java
@@ -0,0 +1,12 @@
+package com.duykypaul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    /*run core spring boot*/
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
