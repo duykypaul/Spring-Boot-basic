c3f8f52fb47a0afa27e0b54d552132380cbef90b
duykypaul <duykypaul@gmail.com>
2020-04-04 23:25:32 +0700

    git edit commondto

--- src/main/java/com/duykypaul/commonAPI.java
+++ src/main/java/com/duykypaul/commonAPI.java
@@ -1,7 +1,7 @@
 package com.duykypaul;
 
 public class commonAPI {
-    private Integer apiCode;
+    private String apiCode;
     private Integer apiCode1;
     private Integer apiCode2;
     private Integer apiCode3;
