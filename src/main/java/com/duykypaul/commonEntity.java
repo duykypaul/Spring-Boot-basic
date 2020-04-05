40a9c91f588346ea47273f948d0580798bc1e474
duykypaul <duykypaul@gmail.com>
2020-04-05 22:26:15 +0700

    git edit commonEntity DTo

--- src/main/java/com/duykypaul/commonEntity.java
+++ src/main/java/com/duykypaul/commonEntity.java
@@ -2,4 +2,8 @@ package com.duykypaul;
 
 public class commonEntity {
     private Integer entity1;
+    private Integer entity2;
+    private Integer entity3;
+    private Integer entity4;
+    private Integer entity5;
 }
